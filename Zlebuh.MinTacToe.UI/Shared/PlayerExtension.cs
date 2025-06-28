71f54cd71e4a7728fc8b1110ed6999c732655c83
Petr Zlebek <pezlebek@gmail.com>
2025-06-28 23:19:42 +0200

    Game info panel

---
+++ Zlebuh.MinTacToe.UI/Shared/PlayerExtension.cs
@@ -0,0 +1,27 @@
+﻿using Zlebuh.MinTacToe.Model;
+
+namespace Zlebuh.MinTacToe.UI.Shared
+{
+    public static class PlayerExtension
+    {
+        public static string ToDisplayString(this Player? player)
+        {
+            return player switch
+            {
+                Player.X => "red",
+                Player.O => "blue",
+                _ => "no one"
+            };
+        }
+
+        public static string ToColor(this Player? player)
+        {
+            return player switch
+            {
+                Player.X => "#ff0064",
+                Player.O => "#00c8e1",
+                _ => "transparent"
+            };            
+        }    
+    }
+}
