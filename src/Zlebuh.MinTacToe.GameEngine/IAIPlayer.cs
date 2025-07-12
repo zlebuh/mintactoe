726fd97d1a8ae32d68070101818404982dc26ae5
Petr Zlebek <pezlebek@gmail.com>
2025-07-12 10:42:21 +0200

    Removed AIPlayer interface

--- src/Zlebuh.MinTacToe.GameEngine/IAIPlayer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Zlebuh.MinTacToe.GameEngine.Model;
-
-namespace Zlebuh.MinTacToe.GameEngine
-{
-    public interface IAIPlayer
-    {
-        Coordinate MakeMove(Game game, Player player);
-    }
-}
